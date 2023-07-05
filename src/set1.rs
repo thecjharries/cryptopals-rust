06390a48abe5c6187e84d3cd8650a6116b297440
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-05 18:37:06 -0500

    Reuse test for hex_to_base64 as Set 1 Challenge 1 solution

--- src/set1.rs
+++ src/set1.rs
@@ -15,5 +15,20 @@
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
+    use crate::util::hex_to_base64;
+
+    #[test]
+    fn challenge1() {
+        assert_eq!(
+            "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBs\
+             aWtlIGEgcG9pc29ub3VzIG11c2hyb29t",
+            hex_to_base64(
+                "49276d206b696c6c696e6720796f7572\
+                 20627261696e206c696b65206120706f\
+                 69736f6e6f7573206d757368726f6f6d"
+            )
+        );
+    }
+
 }
