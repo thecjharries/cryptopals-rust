8cd036137fbedd8f96d7e44da68203a82afd90e0
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-05 17:59:29 -0500

    Set up the first set

--- src/set1.rs
+++ src/set1.rs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod set1;
-
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
