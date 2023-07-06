423a928b2e82f44bcf22042690be59808e4b9dc6
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-06 15:50:17 -0500

    Test easy path for fixed_xor

--- src/util.rs
+++ src/util.rs
@@ -70,4 +70,13 @@ mod tests {
             )
         );
     }
+
+    #[test]
+    fn fixed_xor_should_xor_same_length_vecs() {
+        let result = fixed_xor(
+            hex_to_bytes("1c0111001f010100061a024b53535009181c"),
+            hex_to_bytes("686974207468652062756c6c277320657965"),
+        );
+        assert_eq!(hex_to_bytes("746865206b696420646f6e277420706c6179"), result);
+    }
 }
