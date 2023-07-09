e84b08e6530e030e8645c4ee13f6fc547e224e24
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-09 08:51:54 -0500

    Test repeating_xor

--- src/util.rs
+++ src/util.rs
@@ -98,6 +98,27 @@ mod tests {
     }
 
     #[test]
+    fn repeating_xor_works() {
+        let result = repeating_xor(
+            "Burning 'em, if you ain't quick and nimble\n\
+             I go crazy when I hear a cymbal"
+                .as_bytes()
+                .to_vec(),
+            "ICE".as_bytes().to_vec(),
+        );
+        assert_eq!(
+            hex::decode(
+                "0b3637272a2b2e63622c2e69692a23693a2a3\
+                 c6324202d623d63343c2a2622632427276527\
+                 2a282b2f20430a652e2c652a3124333a653e2\
+                 b2027630c692b20283165286326302e27282f"
+            )
+            .unwrap(),
+            result
+        );
+    }
+
+    #[test]
     fn get_challenge_data_works() {
         let result = get_challenge_data(4);
         let lines = result.lines().collect::<Vec<&str>>();
