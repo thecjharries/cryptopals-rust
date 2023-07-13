df9b700a1044e160606748669f8f465920037d28
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-13 17:31:17 -0500

    Create PKCS#7 module

--- src/pkcs7.rs
+++ src/pkcs7.rs
@@ -12,12 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pub mod text_score;
-pub mod set1;
-pub mod util;
-pub mod aes;
-pub mod pkcs7;
-
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
