2d2b10441e726cf1c262e5d27dce066c59ecacf6
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-16 11:09:37 -0500

    Implement challenge 12

--- src/set2.rs
+++ src/set2.rs
@@ -88,6 +88,31 @@ pub fn detect_block_size(oracle: fn(Vec<u8>, u64) -> Vec<u8>, seed: u64) -> usiz
     block_size
 }
 
+pub fn crack_challenge_12_oracle() -> Vec<u8> {
+    let block_size = detect_block_size(challenge_12_oracle, 0);
+    let original_length = challenge_12_oracle(vec![], 0).len();
+    let mut plaintext = vec![];
+    while plaintext.len() < original_length {
+        let block_start = plaintext.len();
+        let block_end = (block_start + block_size).min(original_length);
+        for length in (0..block_size).rev() {
+            let mut input = vec!['A' as u8; length];
+            let target = challenge_12_oracle(input.clone(), 0)[block_start..block_end].to_vec();
+            input.extend(plaintext.clone());
+            for byte in 0..=255 {
+                let mut input = input.clone();
+                input.push(byte);
+                let output = challenge_12_oracle(input, 0)[block_start..block_end].to_vec();
+                if output == target {
+                    plaintext.push(byte);
+                    break;
+                }
+            }
+        }
+    }
+    plaintext
+}
+
 #[cfg(not(tarpaulin_include))]
 #[cfg(test)]
 mod tests {
@@ -172,5 +197,16 @@ mod tests {
     fn detect_block_size_works() {
         let block_size = detect_block_size(challenge_12_oracle, 0);
         assert_eq!(16, block_size);
+    }
+
+    #[test]
+    fn challenge_12() {
+        let unknown_string = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK".to_string();
+        let unknown_data = general_purpose::STANDARD
+            .decode(unknown_string.as_bytes().to_vec())
+            .unwrap();
+        assert!(String::from_utf8(crack_challenge_12_oracle())
+            .unwrap()
+            .starts_with(String::from_utf8(unknown_data).unwrap().as_str()));
     }
 }
