97b02e15d3506959bac44f718a393838ff661b89
CJ Harries <cj@wizardsoftheweb.pro>
2023-07-21 20:00:55 -0500

    Stub fnc to get random plaintext

--- src/set3/challenge17.rs
+++ src/set3/challenge17.rs
@@ -11,3 +11,42 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+use rand::seq::SliceRandom;
+use rand::RngCore;
+
+fn get_plaintext<R: RngCore>(rng: &mut R) -> Vec<u8> {
+    let plaintexts = vec![
+        "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw=="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg=="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl"
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA=="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw=="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g="
+            .as_bytes()
+            .to_vec(),
+        "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93"
+            .as_bytes()
+            .to_vec(),
+    ];
+    todo!()
+}
